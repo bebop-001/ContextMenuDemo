b652224e55a70b5e1448dd9fd823ce3357697203
sjs <sjs@sonic.net>
2020-09-01 21:03:00 -0700

    add licensing header.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ContextMenuDemo
 Example of a simple Android ContextMenu in Kotlin and Android.
+
+This is the result of a
+*[question I asked on Stackoverflow](https://stackoverflow.com/questions/63696170/convert-java-to-kotlin-breaks-contextmenu)*.  I was trying to move
+a bunch of code from Java to Kotlin and had a hard time finding
+examples.  This is the result.  The master branch is the example
+in Kotlin and the Java branch is the example in Java.
+
+Long-press to access the context menu.
+
+Have fun.<br>
+Steve S.
+
+---
+
+[<img src="./screenshots/noMenu.png"
+	alt="App screenshot"
+	height="640" />]
+[<img src="./screenshots/withMenu.png"
+	alt="App screenshot"
+	height="640" />]
--- app/src/main/java/com/example/contextmenudemo/MainActivity.java
+++ app/src/main/java/com/example/contextmenudemo/MainActivity.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright 2020 Steven Smith kana-tutor.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *
+ *  You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *  either express or implied.
+ *
+ *  See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
 package com.example.contextmenudemo;
 
 import androidx.annotation.Nullable;
@@ -23,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
-                                    @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+                @Nullable ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         Log.d("onCreateContextMenu", "menuInfo:" +
                 ((menuInfo == null) ? "NULL" : menuInfo.toString()));
@@ -48,8 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
             default:
                 Log.d("onContextItemSelected", String.format(
-                        "menu item unhandled:0x%08x", itemId)
-                );
+                    "menu item unhandled:0x%08x", itemId));
                 rv = false;
         }
         return rv;
